061ef069030e2c6ac0b0b8e47574ce3f1b8b2fef
whyy <icarovsilva1@gmail.com>
2024-07-23 15:40:20 -0300

    feat: removendo logs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 export { Query } from "./lib/query";
 import { Query } from "./lib/query";
 
-const queryBuilder = new Query();
-
-const query = queryBuilder.contains("name", "Test").build();
-
-console.log(query);
 
 export default Query;
